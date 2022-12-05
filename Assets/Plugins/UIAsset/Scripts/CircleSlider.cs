2823b7d69cdc0f941c0c2cc2c46c0c6a30543e22
sjmuvc <113957473+sjmuvc@users.noreply.github.com>
2022-12-05 11:52:02 +0900

    3

---
+++ Assets/Plugins/UIAsset/Scripts/CircleSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CircleSlider : MonoBehaviour
+{
+
+    public bool b = true;
+    public Image image;
+    //public float speed = 0.5f;
+    //public string unit = "°C";
+    //float time = 0f;
+    public float range1, range2;
+    private bool up = true;
+    private float amount;
+    public double value;
+
+    //public Text progress;
+
+    private void Update()
+    {
+        float range = (range2 - range1);
+        amount = (float)value / range;
+        image.fillAmount = amount;
+    }
+
+}
+
+
